ee80765264f9f2deadf99a16e91c1b9c8ae9def3
Subrat Kumar <subrat.dataengineer@gmail.com>
2022-12-06 18:28:01 +0000

    power linear using recursion.

---
+++ pep-coding/1-introduction-to-recursion/5-power-linear.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+/** 
+@author: Subrat Kumar
+**/
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int x = scn.nextInt();
+        int n = scn.nextInt();
+        System.out.println(power(x, n));
+    }
+
+    public static int power(int x, int n){
+        if (n == 0) return 1;
+        int xpnm1 = power(x, n - 1); // faith
+        int xpn = x * xpnm1; // expectation
+        return xpn;
+    }
+
+}
