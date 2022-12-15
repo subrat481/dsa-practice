9e78861658ba108da45d661df7457def8b0a1a6a
Subrat Kumar <subrat.dataengineer@gmail.com>
2022-12-15 17:21:55 +0000

    print maze paths recursion

---
+++ pep-coding/2-Recursion and Backtracking/23-print-maze-paths.java
@@ -0,0 +1,45 @@
+import java.io.*;
+	import java.util.*;
+
+	public class Main {
+
+	    public static void main(String[] args) throws Exception {
+	        
+            Scanner scn = new Scanner(System.in);
+            int n = scn.nextInt();
+            int m = scn.nextInt();
+            printMazePaths(1, 1, n, m, "");
+            
+	    }
+        
+        // Labels and Options Style Approach
+        // This is called as Recursion on the way up
+        // Memory Optimized - (n+1)*n
+        // Previously we have taken (2^n)*n in FE (faith-expectation) kind of recursion.
+	    // sr - source row
+	    // sc - source column
+	    // dr - destination row
+	    // dc - destination column
+	    public static void printMazePaths(int sr, int sc, int dr, int dc, String psf) {
+	        
+	        // Handle Smartly
+	        // Don't go out of the board
+	        if (sr > dr || sc > dc) {
+	            return; // don't do anything, simply return
+	        }
+	        
+	        // Smart Base case
+	        // When we reached rhe destination
+	        if (sr == dr && sc == dc) {
+	            System.out.println(psf);
+	            return;
+	        }
+	        
+	        // There are only 2 options - horizontal and vertical
+	        // Call Randomly/Stupidly and Base case smartly
+	        printMazePaths(sr, sc + 1, dr, dc, psf + "h"); // Horizontal call
+	        printMazePaths(sr + 1, sc, dr, dc, psf + "v"); // Vertical call
+	        
+	    }
+
+	}
